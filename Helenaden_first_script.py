ac234125eaf272eed794e3e6c8a1a2c5f8c87243
Helen Dennis <48349365+Helenaden@users.noreply.github.com>
2021-08-04 23:24:32 +0100

    Add files via upload
    Helenaden's script

---
+++ Helenaden_first_script.py
@@ -0,0 +1,11 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Name = "Helen Dennis"
+>>> Email = "Helendennis4u@gmail.com"
+>>> Slack_Username = "Helenaden"
+>>> Biostack = "@Bioinformatics"
+>>> Twitter_Handle = "Helendeee"
+>>> Hamming_Distance = "3"
+>>> print (Name,',',Email,',',Slack_Username,',',Biostack,',',Twitter_Handle,',',Hamming_Distance)
+Helen Dennis , Helendennis4u@gmail.com , Helenaden , @Bioinformatics , Helendeee , 3
+>>> 
