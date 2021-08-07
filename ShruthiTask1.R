5381cfb1b36f16f3868d095431454c4625ed0ae3
knownasshruthi <88314321+knownasshruthi@users.noreply.github.com>
2021-08-07 16:31:25 +0530

    Add files via upload

---
+++ ShruthiTask1.R
@@ -0,0 +1,18 @@
+> Name<-"ShruthiSantosh"
+> Email<-"shruthisantoshpks@gmail.com"
+> Slack_username<-"Shruthi S"
+> Biostack<- "@drugdevelopment"
+> Twitter_handle<-"shruthi10"
+> Hamming_distance<-"3"
+> print(Name)
+[1] "ShruthiSantosh"
+> print(Email)
+[1] "shruthisantoshpks@gmail.com"
+> print(Slack_username)
+[1] "Shruthi S"
+> print(Biostack)
+[1] "@drugdevelopment"
+> print(Twitter_handle)
+[1] "shruthi10"
+> print(Hamming_distance)
+[1] "3"
