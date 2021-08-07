58e09d66af50732e6a152d5d77adeac27c270d63
knownassadhana <88314508+knownassadhana@users.noreply.github.com>
2021-08-07 23:55:50 +0530

    Update koch_script.sh

--- koch_script.sh
+++ koch_script.sh
@@ -1,10 +1,10 @@
-$ cd '/d/koch hackbio'
-$ cat global_csv.sh
-$ #!/usr/bin/env bash
-$ git clone https://github.com/knownasshruthi/koch.git
-$ cd global_csv.sh
-$ chmod +x koch*
-$ for i in $(ls koch*)
-> do
-> ./$i | awk -F '.' '{print $1,$2,$3,$4,$5}' >> Team_koch.csv
-> done
+cd '/d/koch hackbio'
+cat global_csv.sh
+#!/usr/bin/env bash
+git clone https://github.com/knownasshruthi/koch.git
+cd global_csv.sh
+chmod +x koch*
+for i in $(ls koch*)
+do
+./$i | awk -F '.' '{print $1,$2,$3,$4,$5}' >> Team_koch.csv
+done
