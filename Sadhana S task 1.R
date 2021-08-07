3202ccf360ed9bafab1510952817ec3e087843a5
knownassadhana <88314508+knownassadhana@users.noreply.github.com>
2021-08-07 16:50:12 +0530

    Add files via upload

---
+++ Sadhana S task 1.R
@@ -0,0 +1,18 @@
+> Name<-"Sadhana S"
+> EmailID<-"sadhana.thenmozhi@gmail.com"
+> Slack_username<-"@sadhanaS08"
+> Biostack<-"genomics"
+> Twitter_handle<-"@sadhanaS25"
+> Hamming_distance<-"2"
+> print(Name)
+[1] "Sadhana S"
+> print(EmailID)
+[1] "sadhana.thenmozhi@gmail.com"
+> print(Slack_username)
+[1] "@sadhanaS08"
+> print(Biostack)
+[1] "genomics"
+> print(Twitter_handle)
+[1] "@sadhanaS25"
+> print(Hamming_distance)
+[1] "2"
